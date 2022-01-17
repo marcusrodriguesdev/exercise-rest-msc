7a5ffa30adc653ad7802cd79d06c637513c21d88
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-17 12:02:10 -0300

    controllers - findAllProducts and findProductById

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -9,6 +9,23 @@ const createProduct = async (_req, res) => {
   return res.status(201).json(product);
 };
 
+const findAllProducts = async (_req, res) => {
+  const products = await Service.findAllProducts();
+
+  return res.status(200).json({ products });
+};
+
+const findProductById = async (_req, res) => {
+  const { id } = _req.params;
+  const product = await Service.findProductById(id);
+
+  if (product.err) return res.status(422).json({ err: product.err });
+
+  return res.status(200).json(product);
+};
+
 module.exports = {
   createProduct,
+  findAllProducts,
+  findProductById,
 };
