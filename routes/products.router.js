738ace360a93bc6ef1501dc1c8d7e44bf77f4558
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-17 13:02:13 -0300

    routes - updateProduct

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const {
-  createProduct, findProductById, findAllProducts,
+  createProduct, findProductById, findAllProducts, updateProduct,
 } = require('../controllers/products.controllers');
 
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/products', createProduct);
 router.get('/products', findAllProducts);
 router.get('/products/:id', findProductById);
+router.put('/products/:id', updateProduct);
 
 module.exports = router;
