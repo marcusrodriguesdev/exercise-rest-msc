ddf08072e0178b71405d5e3a447c89f9f8c4b7a0
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-17 11:42:30 -0300

    models - findAllProducts and findProductById

--- models/products.models.js
+++ models/products.models.js
@@ -1,4 +1,4 @@
-// const { ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const createProduct = async (id, name, quantity) => {
@@ -15,7 +15,23 @@ const findByName = async (name) => {
   return query;
 };
 
+const findAllProducts = async () => {
+  const db = await connection();
+  const query = await db.collection('products').find({}).toArray();
+
+  return query;
+};
+
+const findProductById = async (id) => {
+  const db = await connection();
+  const query = await db.collection('products').findOne({ _id: ObjectId(id) });
+
+  return query;
+};
+
 module.exports = {
   createProduct,
   findByName,
+  findAllProducts,
+  findProductById,
 };
