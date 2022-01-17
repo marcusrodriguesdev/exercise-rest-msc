392edaf1c3ecfb8ad9d3f1a1789d8c86fc375068
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-17 10:58:33 -0300

    Change name data base

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/ExerciseRest';
-const DB_NAME = 'ExerciseRest';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StorageManager';
+const DB_NAME = 'StorageManager';
 
 module.exports = () => mongodb.connect(MONGO_DB_URL, {
   useNewUrlParser: true,
